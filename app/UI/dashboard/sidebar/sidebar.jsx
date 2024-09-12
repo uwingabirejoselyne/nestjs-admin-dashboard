928ed20de818b376f61ab3f9ef1b0ab3af4ef287
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-09-12 12:04:01 +0200

    react icons installation

--- app/UI/dashboard/sidebar/sidebar.jsx
+++ app/UI/dashboard/sidebar/sidebar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from '../sidebar/sidebar.module.css'
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className={styles.container}>
       Sidebar
     </div>
   )
--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Sidebar from '../UI/dashboard/sidebar/sidebar'
 import Navbar from '../UI/dashboard/navbar/navbar'
+import styles from '../ui/dashboard/dashboard.module.css'
 
 const layout = ({children}) => {
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className ={styles.menu}>
         <Sidebar/>
       </div>
-      <div>
+      <div className={styles.content}>
         <Navbar/>
         {children}
       </div>
