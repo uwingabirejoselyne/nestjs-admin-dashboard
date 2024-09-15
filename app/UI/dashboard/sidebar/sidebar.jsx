bbbbb715b5f299bd8f856364e06351e1ad268d7a
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-09-15 22:46:13 +0200

    created menu item component

--- app/UI/dashboard/sidebar/sidebar.jsx
+++ app/UI/dashboard/sidebar/sidebar.jsx
@@ -1,12 +1,110 @@
-import React from 'react'
-import styles from '../sidebar/sidebar.module.css'
+import React from 'react';
+import styles from '../sidebar/sidebar.module.css';
+import MenuLink from '../sidebar/menuLink/menuLink';
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag, 
+  MdAttachMoney,
+  MdOutlineSettings,
+  MdHelpCenter
+} from 'react-icons/md';
+
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />
+      },
+      {
+        title: "Products",
+        path: "/dashboard/products",
+        icon: <MdShoppingBag />
+      },
+      {
+        title: "Transactions",
+        path: "/dashboard/transactions",
+        icon: <MdAttachMoney />
+      }
+    ]
+  },
+  {
+    title: "Analytics",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdSupervisedUserCircle />
+      },
+      {
+        title: "Products",
+        path: "/dashboard/products",
+        icon: <MdShoppingBag />
+      },
+      {
+        title: "Transactions",
+        path: "/dashboard/transactions",
+        icon: <MdAttachMoney />
+      }
+    ]
+  },
+  {
+    title: "User",
+    list: [
+      {
+        title: "Settings",
+        path: "/dashboard/settings",
+        icon: <MdOutlineSettings />
+      },
+      {
+        title: "Help",
+        path: "/dashboard/help",
+        icon: <MdHelpCenter />
+      },
+      {
+        title: "Products",
+        path: "/dashboard/products",
+        icon: <MdShoppingBag />
+      },
+      {
+        title: "Transactions",
+        path: "/dashboard/transactions",
+        icon: <MdAttachMoney />
+      }
+    ]
+  }
+];
 
 const Sidebar = () => {
   return (
     <div className={styles.container}>
-      Sidebar
+      <ul>
+        {
+          menuItems.map((cat) => (
+            <li key={cat.title}>
+              <span className={styles.cat}>{cat.title}</span>
+                {cat.list.map(item => (
+                    <MenuLink item={item} key={item.title} />
+                ))}
+            </li>
+          ))
+        }
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
