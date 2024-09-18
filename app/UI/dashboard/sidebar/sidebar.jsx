bdcae086cda5dc6db4de7964e8ec14d67fa5eaaa
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-09-18 18:03:56 +0200

    styling side bar

--- app/UI/dashboard/sidebar/sidebar.jsx
+++ app/UI/dashboard/sidebar/sidebar.jsx
@@ -91,7 +91,14 @@ const menuItems = [
 const Sidebar = () => {
   return (
     <div className={styles.container}>
-      <ul>
+      <div className={styles.user}>
+        <img className={styles.userImage} src="/noavatar.png" alt="" width="50" height="50"/>
+        <div className={styles.userDetail}>
+          <span className={styles.username}>Joselyne Uwingabire</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
+      <ul className={styles.list}>
         {
           menuItems.map((cat) => (
             <li key={cat.title}>
